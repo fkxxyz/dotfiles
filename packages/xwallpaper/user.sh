7d7bcf9821827aaccb29018a65e02b1ede840cf2
fkxxyz <fkxxyz@163.com>
2020-08-07 16:42:45 +0800

    update

--- packages/xwallpaper/user.sh
+++ packages/xwallpaper/user.sh
@@ -4,8 +4,8 @@ SCRIPT_FOLDER="$(dirname "$(readlink -f "$0")")"
 
 mkdir -p ~/.local/bin
 
-ln -sf "$SCRIPT_FOLDER/folders_files_switcher" /.local/bin/folders_files_switcher
-ln -sf "$SCRIPT_FOLDER/wallpaper" /.local/bin/wallpaper
+ln -sf "$SCRIPT_FOLDER/folders_files_switcher" ~/.local/bin/folders_files_switcher
+ln -sf "$SCRIPT_FOLDER/wallpaper" ~/.local/bin/wallpaper
 
 install -Dm644 "$SCRIPT_FOLDER/wallpaper.conf" ~/.config/wallpaper.conf
 
